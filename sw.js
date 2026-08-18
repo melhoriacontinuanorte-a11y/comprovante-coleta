97f0b1f077033cff16888465f4b312cf685ec34b
melhoriacontinuanorte-a11y <melhoriacontinuanorte@gmail.com>
2026-08-18 20:53:04 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'coleta-alterosa-v12';
+const CACHE_NAME = 'coleta-alterosa-v13';
 const APP_ASSETS = ['./','./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png','./logo-alterosa.png'];
 self.addEventListener('install', event => { self.skipWaiting(); event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(APP_ASSETS))); });
 self.addEventListener('activate', event => { event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))).then(() => self.clients.claim())); });
